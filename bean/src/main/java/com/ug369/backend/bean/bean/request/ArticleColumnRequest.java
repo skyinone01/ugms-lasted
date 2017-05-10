4ab48ec08d38b5b26a59cd4ef6a24e76c737d368
skyinone <skyinone@163.com>
2017-05-10 17:08:38 +0800

    1 软文管 html编辑器

---
+++ bean/src/main/java/com/ug369/backend/bean/bean/request/ArticleColumnRequest.java
@@ -0,0 +1,142 @@
+package com.ug369.backend.bean.bean.request;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Roy on 2017/5/10.
+ */
+public class ArticleColumnRequest {
+
+    private long id;
+    private String title;
+    private boolean paymode;
+    private PayItem payItem1;
+    private PayItem payItem2;
+    private PayItem payItem3;
+    private List<SimpleArticle> articles = new ArrayList<>();
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isPaymode() {
+        return paymode;
+    }
+
+    public void setPaymode(boolean paymode) {
+        this.paymode = paymode;
+    }
+
+    public PayItem getPayItem1() {
+        return payItem1;
+    }
+
+    public void setPayItem1(int price, int months) {
+        PayItem payItem = new PayItem();
+        payItem.setMonth(months);
+        payItem.setPrice(price);
+        this.payItem1 = payItem;
+    }
+    public void setPayItem2(int price, int months) {
+        PayItem payItem = new PayItem();
+        payItem.setMonth(months);
+        payItem.setPrice(price);
+        this.payItem2 = payItem;
+    }
+    public void setPayItem3(int price, int months) {
+        PayItem payItem = new PayItem();
+        payItem.setMonth(months);
+        payItem.setPrice(price);
+        this.payItem3 = payItem;
+    }
+
+    public void setPayItem1(PayItem payItem1) {
+        this.payItem1 = payItem1;
+    }
+
+    public PayItem getPayItem2() {
+        return payItem2;
+    }
+
+    public void setPayItem2(PayItem payItem2) {
+        this.payItem2 = payItem2;
+    }
+
+    public PayItem getPayItem3() {
+        return payItem3;
+    }
+
+    public void setPayItem3(PayItem payItem3) {
+        this.payItem3 = payItem3;
+    }
+
+    public List<SimpleArticle> getArticles() {
+        return articles;
+    }
+
+    public void getArticles(List<SimpleArticle> articles) {
+        this.articles = articles;
+    }
+
+    public void setArticles(String title,long id) {
+        SimpleArticle article = new SimpleArticle();
+        article.setId(id);
+        article.setTitle(title);
+        articles.add(article);
+    }
+
+    public static class PayItem {
+        private int price;
+        private int month;
+
+        public int getPrice() {
+            return price;
+        }
+
+        public void setPrice(int price) {
+            this.price = price;
+        }
+
+        public int getMonth() {
+            return month;
+        }
+
+        public void setMonth(int month) {
+            this.month = month;
+        }
+    }
+
+    public static class SimpleArticle {
+        private long id;
+        private String title;
+
+        public long getId() {
+            return id;
+        }
+
+        public void setId(long id) {
+            this.id = id;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+    }
+}
--- service/src/main/java/com/ug369/backend/service/service/ArticleService.java
+++ service/src/main/java/com/ug369/backend/service/service/ArticleService.java
@@ -1,16 +1,16 @@
 package com.ug369.backend.service.service;
 
 import com.ug369.backend.bean.base.request.PageRequest;
+import com.ug369.backend.bean.bean.request.ArticleColumnRequest;
 import com.ug369.backend.bean.bean.request.ArticleLabelRequest;
-import com.ug369.backend.bean.bean.request.ArticleLevelEntry;
 import com.ug369.backend.bean.bean.request.ArticleLevelRequest;
 import com.ug369.backend.bean.bean.request.ArticleRequest;
 import com.ug369.backend.bean.exception.UgmsStatus;
 import com.ug369.backend.bean.exception.UserException;
 import com.ug369.backend.bean.result.PagedResult;
-import com.ug369.backend.service.entity.mysql.Article;
-import com.ug369.backend.service.entity.mysql.ArticleCategory;
-import com.ug369.backend.service.entity.mysql.ArticleLabel;
+import com.ug369.backend.service.entity.mysql.*;
+import com.ug369.backend.service.repository.mysql.ArticleColumnReRepository;
+import com.ug369.backend.service.repository.mysql.ArticleColumnRepository;
 import com.ug369.backend.service.repository.mysql.ArticleLabelRepository;
 import com.ug369.backend.service.repository.mysql.ArticleRepository;
 import org.apache.commons.lang3.StringUtils;
@@ -31,6 +31,13 @@ public class ArticleService {
 
     @Autowired
     private ArticleLabelRepository articleLabelRepository;
+
+    @Autowired
+    private ArticleColumnRepository articleColumnRepository;
+
+    @Autowired
+    private ArticleColumnReRepository articleColumnReRepository;
+
 
     @Autowired
     private com.ug369.backend.service.repository.mysql.ArticleLevelRepository articleLevelRepository;
@@ -98,6 +105,7 @@ public class ArticleService {
     }
 
 
+    @Transactional
     public void changeStatus(long id, int op) {
         //审核 1 发布 2 启用 3 停用 4
         //1待审批、2审批通过、3审批不通过、4已发布、5已作作废】
@@ -124,6 +132,7 @@ public class ArticleService {
 
     }
 
+    @Transactional
     public PagedResult<ArticleLabelRequest> getAllLabel(PageRequest pageRequest,String name) {
         Map param = new HashMap<>();
         param.put("name","%"+name+"%");
@@ -131,6 +140,7 @@ public class ArticleService {
 
     }
 
+    @Transactional
     public void addOrUpdateLable(ArticleLabelRequest request){
 
         ArticleLabel label;
@@ -146,6 +156,7 @@ public class ArticleService {
         label.setName(request.getName());
         articleLabelRepository.save(label);
     }
+    @Transactional
     public void deleteLabel(Long id){
         if (id==null || id.longValue()==0){
             throw new UserException(UgmsStatus.BAD_REQUEST);
@@ -153,11 +164,13 @@ public class ArticleService {
         articleLabelRepository.delete(id);
     }
 
+    @Transactional
     public PagedResult<ArticleLevelRequest> getAllLevel(PageRequest pageRequest) {
         return articleLabelRepository.getDataPageBatch("Article.getAllLevel", "Article.getLevelCount", new HashMap<>(), pageRequest);
 
     }
 
+    @Transactional
     public void addOrUpdateLevel(ArticleLevelRequest request){
 
         ArticleCategory level;
@@ -181,16 +194,139 @@ public class ArticleService {
         articleLevelRepository.delete(id);
     }
 
-    public List<ArticleLevelEntry> getLevelList() {
+    @Transactional
+    public PagedResult<ArticleColumn> getAllColumn(PageRequest pageRequest) {
 
-        Iterable<ArticleCategory> all = articleLevelRepository.findAll();
-        List ret = new ArrayList();
-        all.forEach(o->{
-            ArticleLevelEntry entry = new ArticleLevelEntry();
-            entry.setText(o.getName());
-            entry.setValue(o.getId());
-            ret.add(entry);
-        });
-        return ret;
+        return articleLabelRepository.getDataPageBatch("Article.getAllColumn", "Article.getColumnCount", new HashMap<>(), pageRequest);
+
+    }
+    @Transactional
+    public ArticleColumnRequest findColumn(long id) {
+        ArticleColumn one = articleColumnRepository.findOne(id);
+
+        if (one != null){
+            List<Article> byColumn = articleColumnReRepository.getData("Article.getArticleByColumn",one.getId());
+            ArticleColumnRequest result = new ArticleColumnRequest();
+            if (byColumn != null && byColumn.size()>0){
+                byColumn.forEach(o-> result.setArticles(o.getTitle(),o.getId()));
+            }
+
+            result.setId(one.getId());
+            result.setPaymode(one.getPaymode());
+            result.setTitle(one.getTitle());
+            if (one.getPaymode() ){
+                String payitem1 = one.getPayitem1();
+                String[] split = payitem1.split("_");
+                result.setPayItem1(Integer.parseInt(split[0]),Integer.parseInt(split[1]));
+
+                String payitem2 = one.getPayitem2();
+                String[] split2 = payitem2.split("_");
+                result.setPayItem1(Integer.parseInt(split2[0]),Integer.parseInt(split2[1]));
+
+                String payitem3= one.getPayitem3();
+                String[] split3 = payitem3.split("_");
+                result.setPayItem1(Integer.parseInt(split3[0]),Integer.parseInt(split3[1]));
+
+            }
+
+            return result;
+        }
+
+        return null;
+    }
+
+    @Transactional
+    public void deleteColumn(long id) {
+
+        articleColumnReRepository.deleteByColumn(id);
+        articleColumnRepository.delete(id);
+    }
+
+    @Transactional
+    public void changeColumnStatus(Long id, int op) {
+        //审核 1 发布 2 启用 3 停用 4
+        //1待审批、2审批通过、3审批不通过、4已发布、5已作作废】
+        ArticleColumn one = articleColumnRepository.findOne(id);
+        if (one == null){
+            throw  new UserException(UgmsStatus.NOT_FOUND);
+        }
+        if (op ==11){
+            ArticleColumn newone = new ArticleColumn();
+            newone.setStatus(0);
+            newone.setTitle(one.getTitle());
+            newone.setCreatetime(new Date());
+            newone.setPayitem1(one.getPayitem1());
+            newone.setPayitem2(one.getPayitem2());
+            newone.setPayitem3(one.getPayitem3());
+            newone.setPaymode(one.getPaymode());
+            newone.setPicture(one.getPicture());
+            articleColumnRepository.save(newone);
+
+            List<ArticleColumnRe> byColumn = articleColumnReRepository.findByColumn(one.getId());
+            if (byColumn !=null && byColumn.size()>0){
+                byColumn.forEach(o->{
+                    ArticleColumnRe  articleColumnRe = new ArticleColumnRe();
+                    articleColumnRe.setArticle(o.getArticle());
+                    articleColumnRe.setColumn(newone.getId());
+                    articleColumnReRepository.save(articleColumnRe);
+                });
+            }
+        }else {
+            one.setStatus(op);
+            articleColumnRepository.save(one);
+        }
+    }
+
+    @Transactional
+    public void createOrUpdateColumn(ArticleColumn request) {
+
+        ArticleColumn one;
+        if (request.getId()!=null && request.getId() != 0){
+            one = articleColumnRepository.findOne(request.getId());
+            if (one == null){
+                throw new UserException(UgmsStatus.NOT_FOUND);
+            }
+        }else {
+            one = request;
+            one.setCreatetime(new Date());
+        }
+
+        if (request.getStatus()!=null && (request.getStatus()==2 || request.getStatus()==3)){
+            one.setStatus(request.getStatus());
+            one.setApplydetail(request.getApplydetail());
+            one.setApplypeople(request.getApplypeople());
+        }else {
+            one.setStatus(0);
+        }
+        if (request.getPaymode()){
+            one.setPaymode(true);
+            if (!StringUtils.isEmpty(request.getTitle())){
+                one.setTitle(request.getTitle());
+            }
+            if (!StringUtils.isEmpty(request.getPayitem2())){
+                one.setPayitem2(request.getPayitem2());
+            }
+            if (!StringUtils.isEmpty(request.getPayitem3())){
+                one.setPayitem3(request.getPayitem3());
+            }
+        }
+        if (!StringUtils.isEmpty(request.getPicture())){
+            one.setPicture(request.getPicture());
+        }
+
+
+        articleColumnRepository.save(one);
+
+    }
+
+    public void addArticleToColumn(Long column, Long article,boolean addable) {
+        if (addable){
+            ArticleColumnRe one = new ArticleColumnRe();
+            one.setArticle(article);
+            one.setColumn(column);
+            articleColumnReRepository.save(one);
+        }else {
+            articleColumnReRepository.deleteByColumnAndArticle(column,article);
+        }
     }
 }
