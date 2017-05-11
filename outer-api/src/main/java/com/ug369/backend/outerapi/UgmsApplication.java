69f3e971e9d163289df174f06554f1b4f629c71b
spring <hxin_0515@163.com>
2017-05-11 12:55:24 +0800

    1 软文管

--- outer-api/src/main/java/com/ug369/backend/outerapi/UgmsApplication.java
+++ outer-api/src/main/java/com/ug369/backend/outerapi/UgmsApplication.java
@@ -11,6 +11,7 @@ import org.springframework.context.ApplicationContext;
 public class UgmsApplication {
 
     public static void main(String[] args) {
+
         ApplicationContext context = SpringApplication.run(UgmsApplication.class, args);
         ContextUtils.setApplicationContext(context);
 
